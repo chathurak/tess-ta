59eb307579322f5ab71c44ca1a76840ed799f9a0
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-04-30 14:41:28 +0530

    Init project

---
+++ src/main/java/com/languagematters/tessta/TessTaApplication.java
@@ -0,0 +1,13 @@
+package com.languagematters.tessta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TessTaApplication.class, args);
+	}
+
+}
