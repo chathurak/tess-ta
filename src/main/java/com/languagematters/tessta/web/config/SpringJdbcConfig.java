540258b83325a7a21b275479d50881e662433448
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-05-12 19:33:39 +0530

    Remove redundant JDBC configuration bean

--- src/main/java/com/languagematters/tessta/web/config/SpringJdbcConfig.java
+++ src/main/java/com/languagematters/tessta/web/config/SpringJdbcConfig.java
@@ -1,23 +1,23 @@
 package com.languagematters.tessta.web.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class SpringJdbcConfig {
-
-    @Bean
-    public DataSource mysqlDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/tesseract_ta");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-
-        return dataSource;
-    }
-
-}
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class SpringJdbcConfig {
+//
+//    @Bean
+//    public DataSource mysqlDataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/tesseract_ta?autoReconnect=true&useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//
+//        return dataSource;
+//    }
+//
+//}
