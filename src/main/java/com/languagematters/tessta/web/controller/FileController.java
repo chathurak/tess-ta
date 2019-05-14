f047789e852c340658bc7cfe2c722d4753fecfeb
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-05-15 03:10:00 +0530

    Deprecate upload endpoint

--- src/main/java/com/languagematters/tessta/web/controller/FileController.java
+++ src/main/java/com/languagematters/tessta/web/controller/FileController.java
@@ -5,21 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-public class UploadController {
+@RequestMapping("/api/file")
+public class FileController {
 
     private final StorageServices storageServices;
 
     @Autowired
-    public UploadController(final StorageServices storageServices) {
+    public FileController(final StorageServices storageServices) {
         this.storageServices = storageServices;
     }
 
-    @PostMapping("/api/upload/post")
+    @Deprecated
+    @PostMapping("/upload")
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file,
                                                    @RequestParam(value = "pid") String pid) {
         try {
