19f49cc490ef3ce5b1b5a3a6889cbebd8b5b7123
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2019-05-18 14:39:02 +0530

    Create basic file list view

--- src/main/app/src/pages/dashboard/scenes/library/components/filemanagerpanel/FileManagerPanel.jsx
+++ src/main/app/src/pages/dashboard/scenes/library/components/filemanagerpanel/FileManagerPanel.jsx
@@ -2,12 +2,63 @@ import {withStyles} from '@material-ui/core'
 import PropTypes    from 'prop-types'
 import * as React   from 'react'
 import styles       from './styles'
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+
+const ExpansionPanel = withStyles(styles.expansionPanel)(MuiExpansionPanel);
+  
+const ExpansionPanelSummary = withStyles(styles.expansionPanelSummary)(props => <MuiExpansionPanelSummary {...props} />);
+  
+ExpansionPanelSummary.muiName = 'ExpansionPanelSummary';
+  
+const ExpansionPanelDetails = withStyles(styles.expansionPanelDetails)(MuiExpansionPanelDetails);
+  
 
 class FileManagerPanel extends React.Component {
 
+    state = {
+        expanded: '1',
+
+        // TODO: Load data from the server
+        files: [
+            {name: 'File 01', type: 'TEXT', createdOn: '2012.12.12 00:00'},
+            {name: 'File 02', type: 'TIFF', createdOn: '2013.11.12 12:00'},
+        ]
+    };
+
+    handleChange = panel => (event, expanded) => {
+        this.setState({
+            expanded: expanded ? panel : false,
+        });
+    };
+
     render() {
+        const { expanded } = this.state;
+
         return (
             <div className={this.props.className}>
+                {this.state.files.map((file, i) => {         
+                    return (
+                        <ExpansionPanel
+                        square
+                        expanded={expanded === i}
+                        onChange={this.handleChange(i)}
+                        >
+                            <ExpansionPanelSummary>
+                                <Typography>{file.name}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <Typography>
+                                Lorem ipsum dolor sit amet, 
+                                </Typography>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    ) 
+                })}
+
+        
             </div>
         )
     }
--- src/main/app/src/pages/dashboard/scenes/library/components/filemanagerpanel/styles.js
+++ src/main/app/src/pages/dashboard/scenes/library/components/filemanagerpanel/styles.js
@@ -1,3 +1,42 @@
-const styles = theme => ({})
+const styles = {
+    root: { },
+    expansionPanel: {
+        root: {
+          border: '1px solid rgba(0,0,0,.125)',
+          boxShadow: 'none',
+          '&:not(:last-child)': {
+            borderBottom: 0,
+          },
+          '&:before': {
+            display: 'none',
+          },
+        },
+        expanded: {
+          margin: 'auto',
+        },
+      },
+    expansionPanelSummary: {
+        root: {
+          backgroundColor: 'rgba(0,0,0,.03)',
+          borderBottom: '1px solid rgba(0,0,0,.125)',
+          marginBottom: -1,
+          minHeight: 56,
+          '&$expanded': {
+            minHeight: 56,
+          },
+        },
+        content: {
+          '&$expanded': {
+            margin: '12px 0',
+          },
+        },
+        expanded: {},
+      },
+    expansionPanelDetails: {
+        root: {
+          padding: 20,
+        },
+      }
+}
 
 export default styles
