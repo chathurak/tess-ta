e1c34a62b7195286e0b57c077f7bc813f2ec045d
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-11-17 01:27:17 +0530

    Update react
    Fix version issues

--- src/main/app/src/helpers/store.js
+++ src/main/app/src/helpers/store.js
@@ -1,14 +1,14 @@
 import {applyMiddleware, createStore} from 'redux'
-import {createLogger}                 from 'redux-logger'
+// import {createLogger}                 from 'redux-logger'
 import thunkMiddleware                from 'redux-thunk'
 import rootReducer                    from './root-reducer'
 
-const loggerMiddleware = createLogger()
+// const loggerMiddleware = createLogger()
 
 export const store = createStore(
     rootReducer,
     applyMiddleware(
         thunkMiddleware,
-        loggerMiddleware
+        // loggerMiddleware
     )
 )
