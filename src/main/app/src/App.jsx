0f2e11de0ddfb5b56eeddb805ea5c6ed98e785c8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-12-11 11:10:29 +0530

    Apply code split on pages

--- src/main/app/src/App.jsx
+++ src/main/app/src/App.jsx
@@ -1,22 +1,23 @@
 import {withStyles}                      from '@material-ui/core/styles'
 import PropTypes                         from 'prop-types'
-import React                             from 'react'
+import React, {lazy, Suspense}           from 'react'
 import {Redirect, Route, Router, Switch} from 'react-router-dom'
 import styles                            from './App.styles'
 import {history}                         from './helpers'
-import SignIn                            from './pages/signin/SignIn'
-import SignUp                            from './pages/signup/SignUp'
 import {USER}                            from './constants/auth.constants'
 import Header                            from './components/header/Header'
 import Sidebar                           from './components/sidebar/Sidebar'
-import Home                              from './pages/home/Home'
-import Library                           from './pages/library/Library'
-import Ocr                               from './pages/ocr/Ocr'
-import Grammar                           from './pages/grammar/Grammar'
-import Reports                           from './pages/reports/Reports'
-import Settings                          from './pages/settings/Settings'
 import CssBaseline                       from '@material-ui/core/CssBaseline'
 
+const SignIn = lazy(() => import('./pages/signin/SignIn'))
+const SignUp = lazy(() => import('./pages/signup/SignUp'))
+
+const Home = lazy(() => import('./pages/home/Home'))
+const Library = lazy(() => import('./pages/library/Library'))
+const Ocr = lazy(() => import('./pages/ocr/Ocr'))
+const Grammar = lazy(() => import('./pages/grammar/Grammar'))
+const Reports = lazy(() => import('./pages/reports/Reports'))
+const Settings = lazy(() => import('./pages/settings/Settings'))
 
 class App extends React.Component {
 
@@ -36,28 +37,30 @@ class App extends React.Component {
             <div className={classes.root}>
                 <Router history={history}>
                     <div>
-                        <Switch>
-                            <Route path="/signin" component={SignIn}/>
-                            <Route path="/signup" component={SignUp}/>
-                            <Route path="/" render={props => (
-                                localStorage.getItem(USER)
-                                    ? <div className={classes.adminpanel}>
-                                        <CssBaseline/>
-                                        <Header/>
-                                        <Sidebar/>
-                                        <main className={classes.content}>
-                                            <div className={classes.toolbar}/>
-                                            <Route exact path='/home' component={Home}/>
-                                            <Route exact path='/library' component={Library}/>
-                                            <Route exact path='/ocr' component={Ocr}/>
-                                            <Route exact path='/grammar' component={Grammar}/>
-                                            <Route exact path='/reports' component={Reports}/>
-                                            <Route exact path='/settings' component={Settings}/>
-                                        </main>
-                                    </div>
-                                    : <Redirect to={{pathname: '/signin', state: {from: props.location}}}/>
-                            )}/>
-                        </Switch>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Switch>
+                                <Route path="/signin" component={SignIn}/>
+                                <Route path="/signup" component={SignUp}/>
+                                <Route path="/"
+                                       render={props => (localStorage.getItem(USER) ?
+                                               <div className={classes.adminpanel}>
+                                                   <CssBaseline/>
+                                                   <Header/>
+                                                   <Sidebar/>
+                                                   <main className={classes.content}>
+                                                       <div className={classes.toolbar}/>
+                                                       <Route exact path='/home' component={Home}/>
+                                                       <Route exact path='/library' component={Library}/>
+                                                       <Route exact path='/ocr' component={Ocr}/>
+                                                       <Route exact path='/grammar' component={Grammar}/>
+                                                       <Route exact path='/reports' component={Reports}/>
+                                                       <Route exact path='/settings' component={Settings}/>
+                                                   </main>
+                                               </div>
+                                               : <Redirect to={{pathname: '/signin', state: {from: props.location}}}/>
+                                       )}/>
+                            </Switch>
+                        </Suspense>
                     </div>
                 </Router>
             </div>
