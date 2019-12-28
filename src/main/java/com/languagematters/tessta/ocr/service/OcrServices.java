582d2d2343f3e002fc8c2b7e327de78e58a75eaf
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2019-12-29 02:10:52 +0530

    Update tesseract command to support both eng and sin languages
    Closes #122

--- src/main/java/com/languagematters/tessta/ocr/service/OcrServices.java
+++ src/main/java/com/languagematters/tessta/ocr/service/OcrServices.java
@@ -22,7 +22,7 @@ public class OcrServices {
         cmdLine.addArgument(inputPath);
         cmdLine.addArgument(outputPath);
         cmdLine.addArgument("-l");
-        cmdLine.addArgument("sin");
+        cmdLine.addArgument("sin+eng");
         cmdLine.addArgument("segdemo");
         cmdLine.addArgument("inter");
 
