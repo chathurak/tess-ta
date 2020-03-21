957454c092d0540e0a8fc0988c10670d693f6fd3
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-03-21 19:32:13 +0530

    Remove Redis

--- src/main/java/com/languagematters/tessta/controller/GrammarController.java
+++ src/main/java/com/languagematters/tessta/controller/GrammarController.java
@@ -1,21 +1,17 @@
 package com.languagematters.tessta.controller;
 
 import com.languagematters.tessta.grammar.model.WordObj;
-import com.languagematters.tessta.grammar.service.DictionaryService;
 import com.languagematters.tessta.grammar.service.GrammarService;
 import com.languagematters.tessta.grammar.util.FileUtils;
-import com.languagematters.tessta.library.services.DocumentServices;
 import com.languagematters.tessta.library.services.TaskServices;
 import com.languagematters.tessta.security.CurrentUser;
 import com.languagematters.tessta.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,12 +24,9 @@ public class GrammarController {
     private final GrammarService grammarService;
     private final TaskServices taskServices;
 
-    private Jedis jedis = new Jedis("localhost");
-
     @RequestMapping(value = "/api/grammar/process", method = RequestMethod.GET)
     @PreAuthorize("hasRole('USER')")
-    public Map<String, Object> process(@CurrentUser UserPrincipal currentUser,
-                                       @RequestParam(value = "taskId") int taskId) {
+    public Map<String, Object> process(@CurrentUser UserPrincipal currentUser, @RequestParam(value = "taskId") int taskId) {
 
         String text = this.taskServices.getTaskOutputContent(taskId, currentUser.getUsername());
         List<WordObj> result = grammarService.process(text);
@@ -45,21 +38,20 @@ public class GrammarController {
         return res;
     }
 
-    @RequestMapping(value = "/api/grammar/get-test-dir-list", method = RequestMethod.GET)
-    public List<Map<String, String>> getTestDirectoryList() {
-        HashMap<String, Object> map = FileUtils.getTestDirList(jedis.get("TESS_STORAGE_LIBRARY"));
-        List<Map<String, String>> res = new ArrayList<>();
-
-        for (Map.Entry<String, Object> entry : map.entrySet()) {
-            Map<String, String> item = new HashMap<String, String>();
-            item.put("name", entry.getKey());
-            item.put("path", entry.getValue().toString());
-            res.add(item);
-        }
-
-        return res;
-
-    }
+//    @RequestMapping(value = "/api/grammar/get-test-dir-list", method = RequestMethod.GET)
+//    public List<Map<String, String>> getTestDirectoryList() {
+//        HashMap<String, Object> map = FileUtils.getTestDirList(jedis.get("TESS_STORAGE_LIBRARY"));
+//        List<Map<String, String>> res = new ArrayList<>();
+//
+//        for (Map.Entry<String, Object> entry : map.entrySet()) {
+//            Map<String, String> item = new HashMap<>();
+//            item.put("name", entry.getKey());
+//            item.put("path", entry.getValue().toString());
+//            res.add(item);
+//        }
+//
+//        return res;
+//    }
 
     @RequestMapping(value = "/api/grammar/load-recog-output", method = RequestMethod.GET)
     public String loadFile(@RequestParam(value = "testDirPath") String testDirPath) {
