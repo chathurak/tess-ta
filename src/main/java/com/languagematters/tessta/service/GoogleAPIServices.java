8c1e5afb0e8caeadf00ed8c3c342990c6f69fd54
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-04-11 19:40:27 +0530

    Unify Google authorization

--- src/main/java/com/languagematters/tessta/service/GoogleAPIServices.java
+++ src/main/java/com/languagematters/tessta/service/GoogleAPIServices.java
@@ -1,5 +1,11 @@
 package com.languagematters.tessta.service;
 
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -8,9 +14,9 @@ import com.google.api.services.drive.Drive;
 import com.google.api.services.sheets.v4.Sheets;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-
-import static com.languagematters.tessta.util.GoogleAuthorizeUtil.getCredentials;
+import java.util.Collections;
 
 public class GoogleAPIServices {
     private static final String APPLICATION_NAME = "TesseractTA";
@@ -30,6 +36,12 @@ public class GoogleAPIServices {
         return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(accessToken))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+    }
+
+    private static Credential getCredentials(String accessToken) {
+        Credential credential = new Credential(BearerToken.authorizationHeaderAccessMethod());
+        credential.setAccessToken(accessToken);
+        return credential;
     }
 
 }
