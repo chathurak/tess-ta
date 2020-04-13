11cb6b313f4a53fbab261a6e0b94a626f006e273
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-04-13 19:21:47 +0530

    Cleanup AuthController

--- src/main/java/com/languagematters/tessta/controller/AuthController.java
+++ src/main/java/com/languagematters/tessta/controller/AuthController.java
@@ -1,14 +1,10 @@
 package com.languagematters.tessta.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-
-    @PostMapping("/signout")
-    public String signout() {
-        return "index.html";
-    }
 
 }
