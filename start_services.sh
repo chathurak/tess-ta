273d11440c4179211aaa6a7f4eac1b523cd241a9
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2020-05-02 20:19:56 +0530

    Set release option to tessta run command

--- start_services.sh
+++ start_services.sh
@@ -7,10 +7,10 @@
 # Start frontend
 cd ${TESSTA_FE_DIR} && pm2 start yarn --interpreter bash --name api -- start
 
-sleep 40
+sleep 20
 
 # Start backend
-cd ${TESSTA_BE_DIR} && java -jar target/tess-ta-0.0.1-SNAPSHOT.jar > ${LOG_FILE} 2> ${LOG_FILE} &
+cd ${TESSTA_BE_DIR} && java -jar target/tess-ta-0.0.1-SNAPSHOT.jar --spring.profiles.active=release > ${LOG_FILE} 2> ${LOG_FILE} &
 
 tail -f ${LOG_FILE}
 
